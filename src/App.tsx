676ee9ebfd5b558ab4f7b1ef52305c6f7c15e62c
Begimai Mederbekova <begimaialymbekova2@gmail.com>
2024-09-19 04:37:21 +0600

    Initialized the state with the initial tasks.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import './App.css';
+import AddTaskForm from "./components/AddTaskForm.tsx";
+import Task from "./components/Task.tsx";
+import {useState} from "react";
+
+interface ITask {
+    text: string;
+    id: number;
+}
 
 const App = () => {
+    const [tasks, setTasks] = useState<ITask[]>(getInitialTasks);
+
+    const getInitialTasks = () => [
+        {text: 'Read Harry Potter', id: 1},
+        {text: 'Draw a tower', id: 2}
+    ]
+
+
+
+
+
+
 
   return (
     <>
       <div>
+          <AddTaskForm
+
+          />
+          <Task>
+
+          </Task>
 
       </div>
 
